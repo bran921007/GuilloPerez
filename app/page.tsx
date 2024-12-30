e98dfb8254b01b6b99ce72291d6841a5a3341510
Francisco Perez <francisco@square1.io>
2024-12-30 05:47:49 -0400

    improve opengraph and add instagram widget

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,21 @@ import { Partners } from "@/components/home/Partners";
 import { ImageCarousel } from "@/components/carousel/ImageCarousel";
 import { UpcomingExhibitions } from "@/components/exhibitions/UpcomingExhibitions";
 import { ServicesSection } from "@/components/home/ServicesSection";
+import Script from 'next/script'
+
+
+
+function InstagramWidget() {
+  return (
+    <>
+     <Script
+        src="https://static.elfsight.com/platform/platform.js"
+        strategy="lazyOnload"
+      />
+      <div className="elfsight-app-bbb2c86e-006b-4961-b31d-6cc1bbad43d1" data-elfsight-app-lazy></div>
+    </>
+  );
+}
 
 export default function Home() {
   return (
@@ -13,6 +28,7 @@ export default function Home() {
       <Hero />
       <FeaturedArtworks />
       <ServicesSection />
+      <InstagramWidget />
       {/* <UpcomingExhibitions /> */}
       {/* <ImageCarousel /> */}
       <Partners />
